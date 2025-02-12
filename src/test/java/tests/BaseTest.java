aa55dc862bd93734e4132f7eef3a1aa67488a27d
GK <gajendrakanade77@gmail.com>
2025-02-12 15:32:35 +0530

    register Patient file completed

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,28 +3,37 @@ package tests;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class BaseTest {
     WebDriver driver;
-    
+
     @BeforeClass
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "D:\\automation jar files\\chromedriver-win32\\chromedriver.exe");
-        driver = new ChromeDriver();
+//        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--lang=en");
+        driver = new ChromeDriver(options);
+       
+
         driver.manage().window().maximize();
-        driver.get("https://o3.openmrs.org/openmrs/spa/login");
+        driver.get("https://o3.openmrs.org/openmrs/spa/login?lang=en");
     }
-    
+
     @Test(priority = 1)
     public void testLogin() {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -41,152 +50,77 @@ public class BaseTest {
         WebElement loginBtn = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[text()='Log in']")));
         loginBtn.click();
 
-        // Wait for dashboard or confirmation of login success
-        wait.until(ExpectedConditions.urlContains("home"));  // Modify according to actual URL
+        // Validate successful login
+        wait.until(ExpectedConditions.urlContains("home"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("home"), "Login failed!");
         System.out.println("Login successful!");
     }
-    
+
     @Test(priority = 2)
-    public void addPatient() {
+    public void addPatient() throws InterruptedException {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
         // Click on "Add Patient" icon
         WebElement iconClick = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@name='AddPatientIcon']")));
         iconClick.click();
-        
+
         // Enter given name
         WebElement nameField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("givenName")));
         nameField.sendKeys("Virat");
-        
+
         // Enter family name
         WebElement familyName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("familyName")));
         familyName.sendKeys("RCB");
+
+        // Select gender
+        WebElement gender = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(),'Male')]")));
+        gender.click();
+
+        // Enter date of birth
+        WebElement dobField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='spinbutton']")));
+
+        // Scroll into view and click using JavaScript
+        js.executeScript("arguments[0].scrollIntoView(true);", dobField);
+        try {
+            dobField.sendKeys("05072000");
+        } catch (ElementClickInterceptedException e) {
+            System.out.println("ElementClickInterceptedException caught. Using JavaScriptExecutor to click.");
+            js.executeScript("arguments[0].click();", dobField);
+        }
+//        WebElement year = driver.findElement(By.xpath("//input[contains(@id, 'react-aria')]"));
+        Thread.sleep(3000);
         
-	    WebElement gender = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Male']")));
-	    gender.click();
-	    
-	    WebElement dobField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("birthdate")));
-	    dobField.click();
-//	    dobField.sendKeys("01151995"); // Enter the birthdate
-//	    dobField.sendKeys(Keys.ENTER); // Submit the date if required
+        WebElement element = driver.findElement(By.id("phone"));
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+        
+        WebElement regBtn = driver.findElement(By.xpath("//button[text() = 'Register patient']"));
+        regBtn.click();
+        
+        
+//        js = (JavascriptExecutor) driver;
+//
+//     // Clear the field using JavaScript
+//     js.executeScript("arguments[0].value = '';", year);
+//
+//     // Enter the new value
+//     js.executeScript("arguments[0].value = '2000';", year);
 
-	    
-
-    }
-    @Test(priority = 3)
-    public void quitbr() {
-//    	driver.quit();
-    }
+        System.out.println("Patient added successfully!");
     
+
+//    WebElement prev = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label = 'Previous']")));
+//    WebElement nextButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@aria-label='Next']")));
+//    WebElement displayedMonth = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='calendar-header']//span[@class='month']")));
+//    
+//    String targetMonth ="July";
+    
+   
+    }
+    @AfterClass
+    public void quitBrowser() {
+//        if (driver != null) {
+//            driver.quit();
+//        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//package tests;
-//
-//import java.time.Duration;
-//
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.Keys;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//import org.testng.annotations.BeforeClass;
-//import org.testng.annotations.Test;
-//
-//import io.github.bonigarcia.wdm.WebDriverManager;
-//
-////@Test
-//public class BaseTest {
-//	WebDriver driver;
-//	
-//@BeforeClass
-//
-//	public void setup() {
-////		WebDriverManager.chromedriver().setup();
-//		System.setProperty("webdriver.chrome.driver", "D:\\automation jar files\\chromedriver-win32\\chromedriver.exe");
-//		driver = new ChromeDriver();
-//		driver.manage().window().maximize();
-//		driver.get("https://o3.openmrs.org/openmrs/spa/login");
-//		
-//	}
-//	
-//	
-//	@Test(priority = 1)
-//	public void testLogin() throws InterruptedException {
-//	
-//		
-//		WebElement username = driver.findElement(By.id("username"));
-//		username.sendKeys("admin");
-//		Thread.sleep(5000);
-//		
-//		WebElement continueBtn = driver.findElement(By.xpath("//button[text() = \"Continue\"]"));
-//		continueBtn.click();
-//		
-//		WebElement password = driver.findElement(By.id("password"));
-//		password.sendKeys("Admin123");
-//		
-//		WebElement loginBtn = driver.findElement(By.xpath("//button[text() = \"Log in\"]"));
-//		loginBtn.click();
-//		Thread.sleep(5000);
-//	}
-//	
-//	@Test(priority = 2)
-//	public void addPatient() {
-//	    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-//
-//	    WebElement iconclick = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[@name = 'AddPatientIcon']")));
-//	    iconclick.click();
-//	    
-//	    WebElement namefield = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("givenName")));
-//	    namefield.sendKeys("virat");
-//	    
-//	    WebElement familyName = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("familyName")));
-//	    familyName.sendKeys("RCB");
-//	    
-//	    WebElement gender = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text()='Male']")));
-//	    gender.click();
-//	    
-//	    
-//	    WebElement dobField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("birthdate")));
-//	    dobField.clear(); // Clear if pre-filled
-//	    dobField.sendKeys("01151995"); // Enter the birthdate
-//	    dobField.sendKeys(Keys.ENTER); // Submit the date if required
-//
-//	}
-//
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-//	
-////	@Test(priority = 2)
-////	public void addPatient() {
-////		
-////		WebElement iconclick = driver.findElement(By.xpath("//button[@name = \"AddPatientIcon\"]"));
-////		iconclick.click();
-////		
-////		WebElement namefield = driver.findElement(By.id("givenName"));
-////		namefield.sendKeys("virat");
-////	}
-//}
-//
